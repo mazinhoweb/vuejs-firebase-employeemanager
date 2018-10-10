1e060c11b395e09f801111b1fdd4bb72b866863e
Edmar Inocencio (Mazinho Web) <mazinhoweb@outlook.com>
2018-10-10 03:30:57 -0300

    first commit

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyA6DcOevb4ak-xiZfzQ1r-1n45m-iUlGiI",
+    authDomain: "vuefs-prod-237ef.firebaseapp.com",
+    databaseURL: "https://vuefs-prod-237ef.firebaseio.com",
+    projectId: "vuefs-prod-237ef",
+    storageBucket: "vuefs-prod-237ef.appspot.com",
+    messagingSenderId: "542437695600"
+}
